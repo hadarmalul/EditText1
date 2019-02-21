883336f89ef4659955f9fad92f5229ade9a315d7
Arad <ag7393@bs.amalnet.k12.il>
2019-02-21 09:41:56 +0200

    Initial commit

---
+++ app/src/main/java/com/example/user/edittext/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.user.edittext;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    EditText et;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+      et = (EditText) findViewById(R.id.et);
+
+    }
+}
